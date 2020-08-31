218c4ec276b7bb006ceb8159c5de45261e5ad4ca
IhebChatti <iheb.chatti@holbertonschool.com>
2020-08-31 14:09:44 +0100

    python is fun

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""[script that starts a Flask web application]
+"""
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def helloHBNB():
+    """[helloHBNB method]
+
+    Returns:
+        [str]: [returns Hello HBNB!]
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """[HBNB method]
+
+    Returns:
+        [str]: [returns HBNB]
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cIsFun(text):
+    """[C <text> method]
+
+    Args:
+        text ([str]): [the text to be returned after C]
+
+    Returns:
+        [str]: [text to be returned]
+    """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythonIsCool(text='is cool'):
+    """[python routing methid]
+
+    Args:
+        text (str, optional): [text to be put in url]. Defaults to 'is cool'.
+
+    Returns:
+        [str]: [text to be returned]
+    """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', 5000)
