02a5f6d07c81fd2ce24d39980db9d96c210c15d2
IhebChatti <iheb.chatti@holbertonschool.com>
2020-08-31 13:51:14 +0100

    hbnb route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""[script that starts a Flask web application]
+"""
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def helloHBNB():
+    """[helloHBNB method]
+
+    Returns:
+        [str]: [returns Hello HBNB!]
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """[HBNB method]
+
+    Returns:
+        [str]: [returns HBNB]
+    """
+    return 'HBNB'
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', 5000)
